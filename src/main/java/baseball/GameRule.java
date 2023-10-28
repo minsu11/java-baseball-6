f5ccb3873dea527e44b2e9833f99e7237a09a7db
minsu <zmfjtnl1@naver.com>
2023-10-28 22:22:37 +0900

    Create GameRule isCheckNumberOfDigit Method

--- src/main/java/baseball/GameRule.java
+++ src/main/java/baseball/GameRule.java
@@ -1,9 +1,12 @@
 package baseball;
 
+import java.util.regex.Pattern;
+
 public class GameRule {
     /**
      * 숫자 야구 게임의 규칙 및 예외 사항들 모아 놓은 Util class
      */
+    static final Pattern digitPattern = Pattern.compile("^[0-9]{3}$");
 
     private GameRule() {
     }
@@ -18,5 +21,11 @@ public class GameRule {
         return num < 0;
     }
 
+
+    public static void isCheckNumberOfDigit(String num) {
+        if (!digitPattern.matcher(num).find()) {
+            throw new IllegalArgumentException("3자리를 입력해주세요");
+        }
+    }
 
 }
